146954ccd5abde66196faad4aa1745fb08bdd90a
Chris Bowdon <c.bowdon@gmail.com>
2012-03-09 08:06:53 +0000

    stream-mapx now uses dotted tail instead of macro

--- stream-utils.rkt
+++ stream-utils.rkt
@@ -7,20 +7,24 @@
          (contract-out
           [stream-print (-> (or/c stream? list?) any)]))
 
-(define-syntax stream-mapx
-  (syntax-rules ()
-    [(stream-mapx proc a) (stream-map proc a)]
-    [(stream-mapx proc a b ...) (stream-iter proc (list a b ...) empty-stream)]))
 
-(define (stream-iter proc los result)
-  (cond [(ormap stream-empty? los) result]
-        [else
-         (stream-iter
-          proc
-          (map stream-rest los)
-          (stream-append 
-           result
-           (stream-cons (apply proc (map stream-first los)) empty-stream)))]))
+(define (stream-mapx proc a . b)
+  (define (stream-iter proc los result)
+    (cond [(ormap stream-empty? los) result]
+          [else
+           (stream-iter
+            proc
+            (map stream-rest los)
+            (stream-append 
+             result
+             (stream-cons (apply proc (map stream-first los)) empty-stream)))]))
+  (stream-iter proc (cons a b) empty-stream))
+
+; macro to do the same. 
+;(define-syntax stream-mapx
+;  (syntax-rules ()
+;    [(stream-mapx proc a) (stream-map proc a)]
+;    [(stream-mapx proc a b ...) (stream-iter proc (list a b ...) empty-stream)]))
 
 (define (stream-print x)
   (stream-for-each displayln x))
