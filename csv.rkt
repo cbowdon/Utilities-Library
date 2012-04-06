33a51b00a825d6170dfbf2d24097adeff5559b7c
Chris Bowdon <c.bowdon@gmail.com>
2012-04-06 13:37:25 +0100

    csv->stream now uses a generator underneath (ala python)

--- csv.rkt
+++ csv.rkt
@@ -1,22 +1,27 @@
 #lang racket/base
 (require racket/stream
-         racket/port
+         racket/generator
          racket/contract
          "stream-utils.rkt")
 
 (provide (contract-out 
           [csv->stream (-> (or/c string? path?) stream?)]
+          [csv-gen (-> (or/c string? path?) generator?)]
           [split-string (-> string? (or/c string? char?) (listof string?))]
           [string-empty? (-> string? boolean?)]
           [string-not-empty? (-> string? boolean?)]
           [csvs-in-dir (-> (or/c string? path?) (listof path?))])
          select-columns)
 
+;(define (csv->stream filename)
+;  (define (parse-port in)
+;    (stream-map (lambda (x) (parse-by-probable-type (filter string-not-empty? (split-string x ","))))
+;                (port->lines in)))
+;  (call-with-input-file filename parse-port))
+
 (define (csv->stream filename)
-  (define (parse-port in)
-    (stream-map (lambda (x) (parse-by-probable-type (filter string-not-empty? (split-string x ","))))
-                (port->lines in)))
-  (call-with-input-file filename parse-port))
+  (stream-map (lambda (x) (parse-by-probable-type (filter useful-field? (split-string x ","))))
+              (sequence->stream (in-producer (csv-gen filename) 'stop))))
 
 (define (split-string str delimiter)
   (regexp-split delimiter str))
@@ -27,11 +32,17 @@
 (define (string-not-empty? str)
   (not (string-empty? str)))
 
+(define (not-nl? x)
+  (not (or (equal? x "\n") (equal? x "\r") (equal? x "\r\n"))))
+
+(define (useful-field? x)
+  (and (not-nl? x) (string-not-empty? x)))
+
 ; listof string? -> listof int/double/str
 (define (parse-by-probable-type strlis)
   (define (try-string->number str)
     (let ([num (string->number str)])
-      (cond [(number? num) num]
+      (cond [(number? num) num]            
             [else str])))
   (map try-string->number strlis))
 
@@ -51,10 +62,63 @@
    (λ (x) (build-path data-dir x))
    (filter (λ (x) (regexp-match ".csv$" x)) (directory-list data-dir))))
 
+;; csv-gen
+;; a filename ->
+;; a generator that returns successive lines from the file
+(define (csv-gen filename)
+  (generator ()
+             ;; cg-helper
+             ;; pos ->
+             ;; new position and next line
+             (define (cg-helper pos)
+               (define (line-and-pos port)
+                 (begin
+                   (file-position port pos)
+                   (let ([line (read-line port)])
+                     (cons (file-position port) line))))
+               (call-with-input-file filename
+                 (λ (in) (line-and-pos in))))
+             
+             ;; fill-buffer
+             ;; pos ->
+             ;; new position and buffer of lines
+             (define (fill-buffer pos)
+               (define (lines-and-pos port)
+                 (define (l&p-iter count result)
+                   (cond [(= 0 count) (cons (file-position port) (reverse result))]
+                         [else 
+                          (let ([new-line (read-line port)])
+                            ;  (if (eof-object? new-line)
+                            ;     eof
+                            (l&p-iter (sub1 count) (cons new-line result)))]))                 
+                 (begin (file-position port pos)
+                        (l&p-iter 100 '())))
+               (call-with-input-file filename
+                 (λ (in) (lines-and-pos in))))            
+             
+             ;; gen-iter
+             ;; position & buffer ->
+             ;; if eof, null
+             ;; if buffer empty, new position & new buffer
+             ;; else old position and cdr buffer
+             (define (gen-iter old)
+               (cond [(and (pair? (cdr old)) (eof-object? (cadr old))) 'stop]
+                     [(null? (cdr old))
+                      (let ([new (fill-buffer (car old))])
+                        (begin (yield (cadr new))
+                               (gen-iter (cons (car new) (cdr (cdr new))))))]
+                     [else 
+                      (begin (yield (cadr old))
+                             (gen-iter (cons (car old) (cdr (cdr old)))))]))
+             
+             ;; start the gen-iter
+             (gen-iter (cons 0 '()))))
+
+
 ;(require plot)
 ;(define-syntax plot-columns
- ; (syntax-rules ()
-  ;  [(plot-columns strm a b) (plot (points (select-columns strm a b)))]
-   ; [(plot-columns strm a b c) (plot3d (points3d (select-columns strm a b c)))]))
+; (syntax-rules ()
+;  [(plot-columns strm a b) (plot (points (select-columns strm a b)))]
+; [(plot-columns strm a b c) (plot3d (points3d (select-columns strm a b c)))]))
 
 ;(define k (csv->stream "testdata.csv"))
